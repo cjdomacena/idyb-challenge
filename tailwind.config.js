f408f60f8c35a16f3904dea21d88d2145cc22cfd
cjdomacena <cjdomacena@yahoo.com>
2022-04-16 23:11:06 -0400

    chore: :construction: Idk can't think of a good name for this commit... Maybe, minor changes. right. let's go with that

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-bg': '#1B2837',
+        'secondary-bg': '#1A3A53',
+        'tertiary-bg': '#4D6E95',
+        'secondary-text': '#7c8189',
+      },
+    },
   },
   plugins: [],
 };
