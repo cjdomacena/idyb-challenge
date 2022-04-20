9ff2cc7f8bfe6d728aa9b077b5c3834fbf3df242
cjdomacena <cjdomacena@yahoo.com>
2022-04-19 21:00:02 -0400

    feat: :sparkles: Added fetch for slideshow

--- src/components/Slideshow/index.tsx
+++ src/components/Slideshow/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Transition } from '@headlessui/react';
-import {  useRef, useState } from 'react';
+import {  useEffect, useRef, useState } from 'react';
+import { useQuery } from 'react-query';
+import { SteamData } from '../../interface';
+import { getSteamGames } from '../../supabase';
 import Slide from './Slide';
 
 type Props = {};
@@ -10,47 +12,21 @@ type ImageType = {
   id: number;
 };
 
-type SlideshowIndex = {
-  start: number;
-  end: number;
-};
 
 const Slideshow = (props: Props) => {
-  const images: ImageType[] = [
-    {
-      image_url:
-        'https://cdn.akamai.steamstatic.com/steam/apps/915810/capsule_616x353.jpg?t=1649315580',
-      id: 20,
-    },
-    {
-      image_url: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1569040/capsule_616x353.jpg',
-      id: 21,
-    },
-    {
-      image_url: 'https://cdn.akamai.steamstatic.com/steam/apps/570/capsule_616x353.jpg',
-      id: 22,
-    },
-    {
-      image_url: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1203220/capsule_616x353.jpg',
-      id: 23,
-    },
-    {
-      image_url:
-        'https://cdn.akamai.steamstatic.com/steam/apps/915810/capsule_616x353.jpg?t=1649315580',
-      id: 24,
-    },
-    {
-      image_url: 'https://cdn.akamai.steamstatic.com/steam/apps/570/capsule_616x353.jpg',
-      id: 25,
-    },
-  ];
+
   const [slideEndIndex, setSlideEndIndex] = useState<number>(1);
   const [activeIndex, setActiveIndex] = useState<number>(0);
-  const [currentSlides, setCurrentSlides] = useState<ImageType[]>([
-    images[images.length - 1],
-    images[0],
-    images[1],
-  ]);
+  const [images, setImages] = useState<SteamData[] | null | undefined>(null)
+  const [currentSlides, setCurrentSlides] = useState<ImageType[] | undefined>();
+
+  const {data, isFetching} = useQuery('slides', async () => await getSteamGames(5, 'Price'), {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    cacheTime: 1000 * 60,
+    staleTime: 1000 * 120
+  })
+
   const middleRef = useRef(null);
 
   const displaySlides = () => {
@@ -68,47 +44,62 @@ const Slideshow = (props: Props) => {
       return 'right';
     }
   };
+
+  useEffect(() => {
+      if(data && !isFetching) {
+        setImages(data);
+        setCurrentSlides([
+          { image_url: data[data.length - 1].image, id: data[data.length - 1].id },
+          { image_url: data[0].image, id: data[0].id },
+          { image_url: data[2].image, id: data[2].id },
+        ]);
+      }
+
+  }, [data, isFetching])
   
 
-  const handleClickNextSlide = () => {
-    let tempIndex = slideEndIndex;
-    let newSlides = [...currentSlides];
+  // const handleClickNextSlide = () => {
+  //   let tempIndex = slideEndIndex;
+  //   let newSlides = [...currentSlides];
 
-    tempIndex += 1;
-    newSlides.shift();
-    if (slideEndIndex >= images.length - 1) {
-      newSlides.push(images[images.length - 1]);
-      tempIndex = 0;
-      newSlides[2] = images[0];
-    } else {
-      newSlides.push(images[tempIndex]);
-    }
-    setActiveIndex(tempIndex - 1)
-    setSlideEndIndex(tempIndex);
-    setCurrentSlides(newSlides);
-  };
+  //   tempIndex += 1;
+  //   newSlides.shift();
+  //   if (slideEndIndex >= images.length - 1) {
+  //     newSlides.push(images[images.length - 1]);
+  //     tempIndex = 0;
+  //     newSlides[2] = images[0];
+  //   } else {
+  //     newSlides.push(images[tempIndex]);
+  //   }
+  //   setActiveIndex(tempIndex - 1)
+  //   setSlideEndIndex(tempIndex);
+  //   setCurrentSlides(newSlides);
+  // };
 
   const handleMoveSlide = (activeIndex: number) => {
-    let start = 0;
-    let end = 0;
+    if(images) {
+      let start =0;
+      let end = slideEndIndex;
 
-    if (activeIndex - 1 < 0) {
-      start = images.length - 1;
-      end = activeIndex + 1;
-    } else if (activeIndex + 1 > images.length - 1) {
-      end = 0;
-      start = activeIndex - 1;
-    } else {
-      start = activeIndex - 1;
-      end = activeIndex + 1;
+      if (activeIndex - 1 < 0) {
+        start = images.length - 1;
+        end = activeIndex + 1;
+      } else if (activeIndex + 1 > images.length - 1) {
+        end = 0;
+        start = activeIndex - 1;
+      } else {
+        start = activeIndex - 1;
+        end = activeIndex + 1;
+      }
+      const newSlides: ImageType[] = [];
+      newSlides[0] = { image_url: images[start].image , id: images[start].id};
+      newSlides[1] = {image_url: images[activeIndex].image, id:images[activeIndex].id};
+      newSlides[2] = { image_url: images[end].image, id: images[end].id };
+      setCurrentSlides(newSlides);
+      setSlideEndIndex(end);
+      setActiveIndex(activeIndex);
     }
-    const newSlides: ImageType[] = [];
-    newSlides[0] = images[start];
-    newSlides[1] = images[activeIndex];
-    newSlides[2] = images[end];
-    setCurrentSlides(newSlides);
-    setSlideEndIndex(end);
-    setActiveIndex(activeIndex);
+  
   };
 
   return (
@@ -121,7 +112,7 @@ const Slideshow = (props: Props) => {
       </div>
 
       <div className="space-x-4 w-fit mx-auto mt-8 z-50 absolute left-0 right-0 h-fit">
-        {[0, 1, 2, 3,4,5].map((value) => (
+        {[0, 1, 2, 3,4].map((value) => (
           <button
             key={value}
             className={`w-3 h-3 shadow border rounded-full ${
